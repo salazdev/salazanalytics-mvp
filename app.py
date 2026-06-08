8dcb92c0dad8f982796ff03d8ddf71d736485dc0
LUIS SALAZ <97258609+salazdev@users.noreply.github.com>
2026-06-08 17:55:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ def app_principal():
     elif "Factura"      in page: load_module("facturacion",    base/"_facturacion.py").show()
     elif "Flujo"        in page: load_module("flujo_caja",     base/"_flujo_caja.py").show()
     elif "Contabilidad" in page: load_module("contabilidad",   base/"_contabilidad.py").show()
-    elif "Indirecto" in page: load_module("flujo_indirecto", base/"_flujo_indirecto.py").show()
+    elif "Indirecto"    in page: load_module("flujo_indirecto", base/"_flujo_indirecto.py").show()
     elif "Mi Empresa"   in page: pantalla_mi_empresa()
 
 if not st.session_state.get("logged_in"):
